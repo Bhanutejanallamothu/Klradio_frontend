0b0d611a7169ecc52d9ea36a23c17907416fa586
Bhanutejanallamothu <163739352+Bhanutejanallamothu@users.noreply.github.com>
2026-03-04 11:32:02 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://backend-mbht.onrender.com/api',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://klradio-backend.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
